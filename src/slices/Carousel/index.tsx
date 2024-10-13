6a5759b7a0363ca107e083decffba0e53197e963
Faraz Khan <faraz.khan14158124@gmail.com>
2024-10-13 22:52:21 +0530

    Carousel Slice Completed

--- src/slices/Carousel/index.tsx
+++ src/slices/Carousel/index.tsx
@@ -2,12 +2,20 @@
 import { ArrowIcon } from "@/components/ArrowIcon";
 import FloatingCan from "@/components/FloatingCan";
 import { SodaCanProps } from "@/components/SodaCan";
+import { WavyCircles } from "@/components/WavyCircles";
 import { Content } from "@prismicio/client";
 import { PrismicRichText, PrismicText, SliceComponentProps } from "@prismicio/react";
 import { Center, Environment, View } from "@react-three/drei";
 import clsx from "clsx";
-import { useState } from "react";
+import { useRef, useState } from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { Group } from "three";
 
+
+gsap.registerPlugin(useGSAP)
+
+const SPINS_ON_CHANGE = 8;
 const FLAVORS: {
   flavor: SodaCanProps["flavor"];
   color: string;
@@ -34,56 +42,97 @@ export type CarouselProps = SliceComponentProps<Content.CarouselSlice>;
  */
 const Carousel = ({ slice }: CarouselProps): JSX.Element => {
   const [currentFlavorIndex, setCurrentFlavorIndex] = useState(0);
+  const sodaCanRef = useRef<Group>(null);
 
-  const changeFlavor = (index: number) => {
-    const newIndex = (index + FLAVORS.length) % FLAVORS.length;
+  function changeFlavor(index: number) {
+    if (!sodaCanRef.current) return;
 
-    setCurrentFlavorIndex(newIndex);
+    const nextIndex = (index + FLAVORS.length) % FLAVORS.length;
+
+    const tl = gsap.timeline();
+
+    tl.to(
+      sodaCanRef.current.rotation,
+      {
+        y:
+          index > currentFlavorIndex
+            ? `-=${Math.PI * 2 * SPINS_ON_CHANGE}`
+            : `+=${Math.PI * 2 * SPINS_ON_CHANGE}`,
+        ease: "power2.inOut",
+        duration: 1,
+      },
+      0,
+    )
+      .to(
+        ".background, .wavy-circles-outer, .wavy-circles-inner",
+        {
+          backgroundColor: FLAVORS[nextIndex].color,
+          fill: FLAVORS[nextIndex].color,
+          ease: "power2.inOut",
+          duration: 1,
+        },
+        0,
+      )
+      .to(".text-wrapper", { duration: 0.2, y: -10, opacity: 0 }, 0)
+      .to({}, { onStart: () => setCurrentFlavorIndex(nextIndex) }, 0.5)
+      .to(".text-wrapper", { duration: 0.2, y: 0, opacity: 1 }, 0.7);
   }
 
   return (
     <section
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
-      className="carousel relative grid h-screen grid-rows-[auto, 4fr, auto] justify-center overflow-hidden bg-white py-12 text-white"
+      className="carousel relative grid h-screen grid-rows-[auto,4fr,auto] justify-center overflow-hidden bg-white py-12 text-white"
     >
-      <div className="background pointer-events-none absolute inset-0 bg-[#710523] opacity-50"></div>
+      <div className="background pointer-events-none absolute inset-0 bg-[#710523] opacity-50" />
+
+      <WavyCircles className="absolute left-1/2 top-1/2 h-[120vmin] -translate-x-1/2 -translate-y-1/2 text-[#710523]" />
+
       <h2 className="relative text-center text-5xl font-bold">
         <PrismicText field={slice.primary.heading} />
       </h2>
 
-      <div className="grid grid-cols-[auto,auto,auto] items-center ">
-
-        {/* Left Button */}
-        <ArrowButton onClick={() => {
-          changeFlavor(currentFlavorIndex - 1)
-        }} direction="left" label="Previous Flavor" />
-
-        {/* Center Can */}
+      <div className="grid grid-cols-[auto,auto,auto] items-center">
+        {/* Left */}
+        <ArrowButton
+          onClick={() => changeFlavor(currentFlavorIndex + 1)}
+          direction="left"
+          label="Previous Flavor"
+        />
+        {/* Can */}
         <View className="aspect-square h-[70vmin] min-h-40">
           <Center position={[0, 0, 1.5]}>
-            <FloatingCan rotationIntensity={1} floatIntensity={0.3} flavor={FLAVORS[currentFlavorIndex].flavor} />
+            <FloatingCan
+              ref={sodaCanRef}
+              floatIntensity={0.3}
+              rotationIntensity={1}
+              flavor={FLAVORS[currentFlavorIndex].flavor}
+            />
           </Center>
 
-          <Environment files={"/hdrs/lobby.hdr"} environmentIntensity={0.6} environmentRotation={[0, 3, 0]} />
+          <Environment
+            files="/hdrs/lobby.hdr"
+            environmentIntensity={0.6}
+            environmentRotation={[0, 3, 0]}
+          />
           <directionalLight intensity={6} position={[0, 1, 1]} />
         </View>
-
-        {/* Right Button */}
-        <ArrowButton onClick={() => {
-          changeFlavor(currentFlavorIndex + 1)
-        }} direction="right" label="Next Flavor" />
-
+        {/* Right */}
+        <ArrowButton
+          onClick={() => changeFlavor(currentFlavorIndex - 1)}
+          direction="right"
+          label="Next Flavor"
+        />
       </div>
-      <div className="text-area relative mx-auto text-center ">
+
+      <div className="text-area relative mx-auto text-center">
         <div className="text-wrapper text-4xl font-medium">
           <p>{FLAVORS[currentFlavorIndex].name}</p>
         </div>
-        <div className="mt-2 font-normal text-2xl opacity-90">
+        <div className="mt-2 text-2xl font-normal opacity-90">
           <PrismicRichText field={slice.primary.price_copy} />
         </div>
       </div>
-
     </section>
   );
 };
@@ -94,11 +143,20 @@ type ArrowButtonProps = {
   direction?: "right" | "left";
   label: string;
   onClick: () => void;
-}
+};
 
-function ArrowButton({ label, onClick, direction = "right" }: ArrowButtonProps) {
-  return <button className="size-12 rounded-full border-2 border-white bg-white/10 p-3 opacity-80 ring-white focus:outline-none focus-visible:opacity-100 focus-visible:ring-4 hover:bg-white/40 md:size-16 lg:size-20" onClick={onClick}>
-    <ArrowIcon className={clsx(direction === "right" && "-scale-x-100")} />
-    <span className="sr-only">{label}</span>
-  </button>
+function ArrowButton({
+  label,
+  onClick,
+  direction = "right",
+}: ArrowButtonProps) {
+  return (
+    <button
+      onClick={onClick}
+      className="size-12 rounded-full border-2 border-white bg-white/10 p-3 opacity-85 ring-white focus:outline-none focus-visible:opacity-100 focus-visible:ring-4 md:size-16 lg:size-20"
+    >
+      <ArrowIcon className={clsx(direction === "right" && "-scale-x-100")} />
+      <span className="sr-only">{label}</span>
+    </button>
+  );
 }
